7d16a5b01f003040be274535bfc0c5ef2f01b415
SamNewmanSanders <samnewmansanders@gmail.com>
2026-05-18 13:17:47 +0100

    Graphing the matched filter

--- SAM-NS/Recieve_chirp.py
+++ SAM-NS/Recieve_chirp.py
@@ -1,6 +1,8 @@
 from scipy.io import wavfile
 import numpy as np
 from scipy.signal import correlate
+import matplotlib.pyplot as plt
+
 
 fs_rx, rxChirp = wavfile.read("SAM-NS/recieved_chirp.wav")
 fs_tx, txChirp = wavfile.read("SAM-NS/chirp.wav")
@@ -24,3 +26,15 @@ top_100_values = corr_abs[top_100_indices]
 
 for i in range(100):
     print(top_100_indices[i], top_100_values[i])
+
+
+    # Sample indices for x-axis
+x = np.arange(len(corr))
+
+# Plot
+plt.figure()
+plt.plot(x, np.abs(corr))
+plt.title("Matched Filter Output (Correlation)")
+plt.xlabel("Sample index")
+plt.ylabel("Correlation magnitude")
+plt.show()
