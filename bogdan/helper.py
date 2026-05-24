708a6e7ff9d7fdcdd97b2c9d6c44945d1bfb38ff
Bogdan Isaev <57876472+bogdisae@users.noreply.github.com>
2026-05-24 03:31:47 +0100

    noise synchronisation test plus some channel estimation

---
+++ bogdan/helper.py
@@ -0,0 +1,60 @@
+import pickle
+import sounddevice as sd
+import numpy as np
+from scipy.io.wavfile import write
+import matplotlib.pyplot as plt
+
+def cross_correlation(x, k):
+    y = []
+    for i in range(0, len(x)-k):
+        y.append(x[i]*x[i+k])
+    return y
+
+def save_dict(obj, filename):
+    with open(filename, 'wb') as f:
+        pickle.dump(obj, f)
+
+def load_dict(filename):
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
+    
+def record(t=3, savefile = False, filedir='bogdan/recordings/', fs=44100):
+    recording = sd.rec(int(t * fs), samplerate=fs, channels=1).reshape(int(t*fs))
+    sd.wait()
+    if savefile:
+        write(filedir+"output.wav", fs, recording) 
+        np.savetxt(filedir+"output.txt", recording, delimiter="\n") 
+    return recording
+    
+# def write_csv(filename, data, headers=None):
+#     with open(filename, 'w', newline='') as f:
+#         writer = csv.writer(f)
+#         if headers:
+#             writer.writerow(headers)
+#         writer.writerows(data)
+
+# def read_csv(filename, has_headers=True):
+#     with open(filename, 'r', newline='') as f:
+#         reader = csv.reader(f)
+#         if has_headers:
+#             headers = next(reader)
+#             return headers, list(reader)
+#         return None, list(reader)
+
+def synchronisation_plot(recording, correlation, windowed, h, start_index = None):
+    fig, ax = plt.subplots(2, 2, constrained_layout=True)
+    ax[0, 0].plot(recording)
+    ax[0, 0].set_title("a) Channel response")
+    ax[0, 1].plot(correlation)
+    ax[0, 1].set_title("b) Corellation: c[k] = y[x]y[x+d]")
+    ax[1, 0].plot(windowed)
+    ax[1, 0].set_title("c) Rectangular window correlation")
+    ax[1, 1].plot(h)
+    ax[1, 1].set_title("d) Estimated channel")
+
+    if start_index is not None:
+        ax[0, 0].axvline(x=start_index, color='red', linestyle='--', label='start')
+        ax[0, 1].axvline(x=start_index, color='red', linestyle='--', label='start')
+        ax[1, 0].axvline(x=start_index, color='red', linestyle='--', label='start')
+
+    plt.show()
--- bogdan/rx.py
+++ bogdan/rx.py
@@ -1,15 +1,52 @@
-import sounddevice as sd
 import numpy as np
-from scipy.io.wavfile import write
+from helper import cross_correlation
+
+class Rx:
+    signal: np.ndarray
+    correlation_dist: int
+    n_taps: int
+
+    noise_key: np.ndarray
+    synchronisation_index: int
+    windowed: np.ndarray
+    correlation: np.ndarray
+    H: np.ndarray
+    h: np.ndarray
+
+    def __init__(self, signal:np.ndarray, correlation_dist:int, n_taps:int, noise_key: np.ndarray):
+        self.signal = signal
+        self.correlation_dist = correlation_dist
+        self.n_taps = n_taps
+        self.noise_key = noise_key
+
+    def detect_windowed_signal_peak(self):
+        peak_width_estimate = self.n_taps // 5
+        abs_windowed = abs(self.windowed)
+        # First value that lies within the top peak_width_estimate
+        threshold = np.sort(abs_windowed)[-peak_width_estimate]
+        first_index = np.argmax(abs_windowed >= threshold)
+        self.synchronisation_index = first_index - self.correlation_dist
+
+    def synchronise_noise_key(self):
+        self.correlation = cross_correlation(self.signal, self.correlation_dist)
+        self.windowed = np.convolve(self.correlation, np.ones(self.correlation_dist))
+
+        self.detect_windowed_signal_peak()
+
+    def channel_estimate_division(self, received, known):
+        '''Assuming synchronisation, use the known sent signal and the received signal to estimate impulse response'''
+        fftlen = len(received)
+        Y = np.fft.fft(received, n = fftlen)
+        X = np.fft.fft(known, n = fftlen)
+        self.H = Y/X
+        self.h = np.fft.ifft(self.H)
+
+    def channel_estimate(self):
+        y = self.signal[self.synchronisation_index: self.synchronisation_index + self.correlation_dist]
+        self.channel_estimate_division(y, self.noise_key)
+    
+    def decode(self):
+        self.synchronise_noise_key()
+        self.channel_estimate()
 
 
-def record(t=3, savefile = False, filedir='bogdan/recordings/', fs=44100):
-    recording = sd.rec(int(t * fs), samplerate=fs, channels=1)
-    sd.wait()
-    if savefile:
-        write(filedir+"output.wav", fs, recording) 
-        np.savetxt(filedir+"output.txt", recording, delimiter="\n") 
-    return recording
-
-recording = record(savefile=True)
-a = 2
