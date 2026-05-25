7d4e763d21a6569d8d2bfb23e9fb20a26bfeed06
Aaron MacWhirter <am3204@cam.ac.uk>
2026-05-25 10:31:28 +0100

    Folder branch, but not actually a branch

---
+++ Main Pipeline 2/constellation.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class Constellation:
+    bits_per_symbol: int
+    constellation: dict
+    def __init__(self, bits_per_symbol, constellation):
+        self.bits_per_symbol = bits_per_symbol
+        self.constellation = constellation
+    
+    def bits_to_symbols(self, bits):
+        if len(bits)%self.bits_per_symbol!=0:
+            raise Exception(f"Bit string not divisible by {self.bits_per_symbol}")
+            # Pad bits instead?
+        group_bits = [tuple(bits[i:i+self.bits_per_symbol]) for i in range(0, len(bits), self.bits_per_symbol)]
+        return np.array([self.constellation[b] for b in group_bits], dtype=complex) # could be made faster with numpy
+    
+    def symbols_to_bits(self, symbols):
+        pass
